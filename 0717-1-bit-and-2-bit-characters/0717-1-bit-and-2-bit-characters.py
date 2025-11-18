9f473a59641131bd31d7ea15c3ae17b36eb2a79b
Kasireddy Satya Sharanya <168414847+sharanyakasireddy@users.noreply.github.com>
2025-11-19 00:24:48 +0530

    Time: 0 ms (100%), Space: 17.8 MB (67.66%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        n = len(bits)
+        while i < n - 1:      # process until the second-last bit
+            if bits[i] == 1:
+                i += 2        # 2-bit character
+            else:
+                i += 1        # 1-bit character
+        return i == n - 1   
