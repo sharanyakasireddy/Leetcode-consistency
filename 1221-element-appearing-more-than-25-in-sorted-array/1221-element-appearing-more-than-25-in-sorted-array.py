b818035e0909ae7c1ce59066c40c5fac78515744
Kasireddy Satya Sharanya <168414847+sharanyakasireddy@users.noreply.github.com>
2025-11-19 23:30:22 +0530

    Time: 0 ms (100%), Space: 18.5 MB (91.37%) - LeetHub

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        quarter = n // 4
+
+        for i in range(n - quarter):
+            if arr[i] == arr[i + quarter]:
+                return arr[i]
+
+        return arr[0]   # fallback (but loop always returns before this)
