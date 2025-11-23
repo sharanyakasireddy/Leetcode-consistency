0077c55010282143faad91a7a6ce6563a3de4e74
Kasireddy Satya Sharanya <168414847+sharanyakasireddy@users.noreply.github.com>
2025-11-24 00:28:21 +0530

    Time: 768 ms (13.02%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,12 @@
-# Write your MySQL query statement below
 SELECT 
     user_id,
     CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
 FROM Users
 ORDER BY user_id;
+
+
+#LEFT(name,1) → first character
+#UPPER() → convert first character to capital
+#SUBSTRING(name,2) → remaining characters
+#LOWER() → convert remaining to lowercase
+#CONCAT() → join both
