e592053098d53c3d0beb0a372d09260d70d70c79
Kasireddy Satya Sharanya <168414847+sharanyakasireddy@users.noreply.github.com>
2025-12-07 21:53:34 +0530

    Time: 47 ms (7.49%), Space: 17.8 MB (49.37%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -4,3 +4,12 @@ class Solution:
         if low % 2 == 1 or high % 2 == 1:
             count += 1
         return count
+        
+        
+        
+        #if low%2==1 and high%2==1:
+        #    diff=high-low+1
+        #    return diff//2+1
+        #else:
+        #   diff=high-low+1
+        #    return diff//2
